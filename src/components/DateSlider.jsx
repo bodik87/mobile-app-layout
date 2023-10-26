28a2207b131970d35704cd5cb4b3b2b799799aed
bogdan shulika <bsshul@gmail.com>
2023-10-26 17:21:06 +0300

    Style

--- src/components/DateSlider.jsx
+++ src/components/DateSlider.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
+import { DayPicker } from "react-day-picker";
 import { addDays, eachDayOfInterval, format, subDays } from "date-fns";
 import { uk } from "date-fns/locale";
-import { DayPicker } from "react-day-picker";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { HomeIcon, LoginIcon, SearchBottomIcon, SettingsIcon } from "./Icons";
+import {
+  CalendarIcon,
+  HomeIcon,
+  LoginIcon,
+  SearchBottomIcon,
+  SettingsIcon,
+} from "./Icons";
 import "swiper/css";
 import "react-day-picker/dist/style.css";
 
 export default function DateSlider() {
-  const initialSlide = "99";
   const [counter, setCounter] = useState(0);
   const [visibleCalendar, setVisibleCalendar] = useState(false);
   const [selectedDay, setSelectedDay] = useState();
@@ -18,12 +23,16 @@ export default function DateSlider() {
   }, [selectedDay]);
 
   const days = eachDayOfInterval({
-    start: subDays(selectedDay || new Date(), 100),
+    start: subDays(selectedDay || new Date(), 30),
     end: addDays(selectedDay || new Date(), counter),
   });
 
   return (
-    <section className="bg-red-100 h-full flex flex-col">
+    <section className="h-full flex flex-col">
+      <header className="h-[60px] flex items-center px-5 font-semibold">
+        HEADER
+      </header>
+
       {visibleCalendar && (
         <>
           <DayPicker
@@ -32,7 +41,7 @@ export default function DateSlider() {
             onSelect={setSelectedDay}
             showOutsideDays
             locale={uk}
-            className="absolute left-0 right-0 w-fit mx-auto bottom-[80px] z-20 bg-slate-200 p-5 rounded-xl shadow-xl capitalize"
+            className="absolute left-0 right-0 w-fit mx-auto bottom-[80px] z-20 bg-gray-200 p-5 rounded-xl shadow-xl capitalize select-none"
           />
           <div
             onClick={() => setVisibleCalendar(false)}
@@ -42,10 +51,10 @@ export default function DateSlider() {
       )}
 
       <Swiper
-        initialSlide={initialSlide}
+        initialSlide={"29"}
         spaceBetween={0}
         slidesPerView={1}
-        className="bg-slate-300 w-full grow"
+        className="w-full grow"
         onSlideChange={(swiper) => {
           if (swiper.previousIndex < swiper.activeIndex) {
             swiper.previousIndex !== 0 &&
@@ -62,18 +71,31 @@ export default function DateSlider() {
       >
         {days.slice(1).map((item) => (
           <SwiperSlide key={item}>
-            <div key={item} className="p-3 flex justify-center items-center">
-              <p className="font-medium capitalize select-none text-center">
-                {format(selectedDay || new Date(), "EEEE dd.MM.Y", {
+            <div
+              key={item}
+              className="p-3 flex flex-col justify-center items-center"
+            >
+              <p className="font-semibold capitalize select-none text-center">
+                {format(selectedDay || new Date(), "EEEE dd.MM.yy", {
                   locale: uk,
                 })}
               </p>
+
+              <div className="mt-5 rounded-xl bg-gray-200 p-10 w-full">
+                CONTENT
+              </div>
+              <div className="mt-5 rounded-xl bg-yellow-200 p-10 w-full">
+                CONTENT
+              </div>
+              <div className="mt-5 rounded-xl bg-violet-200 p-10 w-full">
+                CONTENT
+              </div>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
 
-      <footer className="h-[60px] flex justify-around bg-gray-500">
+      <footer className="h-[60px] flex justify-around bg-gray-200">
         <button className="p-4 active:scale-95 transition-all">
           <HomeIcon />
         </button>
@@ -94,19 +116,5 @@ export default function DateSlider() {
         </button>
       </footer>
     </section>
-  );
-}
-
-export function CalendarIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-      <path
-        stroke="#fff"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M3 10h18M7 3v2m10-2v2M6.2 21h11.6c1.12 0 1.68 0 2.108-.218a2 2 0 0 0 .874-.874C21 19.48 21 18.92 21 17.8V8.2c0-1.12 0-1.68-.218-2.108a2 2 0 0 0-.874-.874C19.48 5 18.92 5 17.8 5H6.2c-1.12 0-1.68 0-2.108.218a2 2 0 0 0-.874.874C3 6.52 3 7.08 3 8.2v9.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874C4.52 21 5.08 21 6.2 21Z"
-      />
-    </svg>
   );
 }
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,16 +1,13 @@
+/* eslint-disable react/prop-types */
 export function ThemeIcon({ active = false }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none">
       <path
-        className={`${
-          active ? "fill-white dark:fill-black" : "fill-white dark:fill-black"
-        }`}
+        className={`${active ? "fill-white" : "fill-white"}`}
         d="M15 10a5 5 0 0 0 0 10V10Z"
       />
       <path
-        className={`${
-          active ? "fill-white dark:fill-black" : "fill-white dark:fill-black"
-        }`}
+        className={`${active ? "fill-white" : "fill-white"}`}
         fillRule="evenodd"
         d="M15 27.5c6.904 0 12.5-5.596 12.5-12.5 0-6.903-5.596-12.5-12.5-12.5C8.097 2.5 2.5 8.097 2.5 15c0 6.904 5.597 12.5 12.5 12.5Zm0-2.5v-5a5 5 0 0 0 0-10V5C9.477 5 5 9.477 5 15s4.477 10 10 10Z"
         clipRule="evenodd"
@@ -26,7 +23,7 @@ export function Loader() {
       xmlSpace="preserve"
       viewBox="0 0 54 20"
     >
-      <circle cx="6" cy="10" r="6" className="fill-black dark:fill-yellow">
+      <circle cx="6" cy="10" r="6" className="fill-black">
         <animate
           attributeName="opacity"
           begin=".1"
@@ -35,7 +32,7 @@ export function Loader() {
           values="0;1;0"
         />
       </circle>
-      <circle cx="26" cy="10" r="6" className="fill-black dark:fill-yellow">
+      <circle cx="26" cy="10" r="6" className="fill-black">
         <animate
           attributeName="opacity"
           begin=".2"
@@ -44,7 +41,7 @@ export function Loader() {
           values="0;1;0"
         />
       </circle>
-      <circle cx="46" cy="10" r="6" className="fill-black dark:fill-yellow">
+      <circle cx="46" cy="10" r="6" className="fill-black">
         <animate
           attributeName="opacity"
           begin=".3"
@@ -61,11 +58,7 @@ export function SearchBottomIcon({ active = false }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
       <path
-        className={`${
-          active
-            ? "stroke-black dark:stroke-yellow"
-            : "stroke-gray-300 dark:stroke-gray-300"
-        }`}
+        className={`${active ? "stroke-black" : "stroke-gray-600"}`}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
@@ -79,11 +72,7 @@ export function HomeIcon({ active = false }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
       <path
-        className={`${
-          active
-            ? "stroke-black dark:stroke-yellow"
-            : "stroke-gray-300 dark:stroke-gray-300"
-        }`}
+        className={`${active ? "stroke-black" : "stroke-gray-600"}`}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
@@ -97,21 +86,13 @@ export function SettingsIcon({ active = false }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none">
       <path
-        className={`${
-          active
-            ? "fill-black dark:fill-yellow"
-            : "fill-gray-300 dark:fill-gray-300"
-        }`}
+        className={`${active ? "fill-black" : "fill-gray-600"}`}
         fillRule="evenodd"
         d="M9 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 2a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"
         clipRule="evenodd"
       />
       <path
-        className={`${
-          active
-            ? "fill-black dark:fill-yellow"
-            : "fill-gray-300 dark:fill-gray-300"
-        }`}
+        className={`${active ? "fill-black" : "fill-gray-600"}`}
         fillRule="evenodd"
         d="M15.66 6.155 9 2.309 2.34 6.155v7.69L9 17.691l6.66-3.846v-7.69ZM10 .577a2 2 0 0 0-2 0L1.34 4.423a2 2 0 0 0-1 1.732v7.69a2 2 0 0 0 1 1.732L8 19.423a2 2 0 0 0 2 0l6.66-3.846a2 2 0 0 0 1-1.732v-7.69a2 2 0 0 0-1-1.732L10 .577Z"
         clipRule="evenodd"
@@ -124,11 +105,7 @@ export function LoginIcon({ active = false }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
       <path
-        className={`${
-          active
-            ? "fill-black dark:fill-yellow"
-            : "fill-gray-300 dark:fill-gray-300"
-        }`}
+        className={`${active ? "fill-black" : "fill-gray-600"}`}
         fillRule="evenodd"
         d="M12 4a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM7 7a5 5 0 1 1 10 0A5 5 0 0 1 7 7Zm1.13 8a2.11 2.11 0 0 0-.338.014 1.421 1.421 0 0 0-.148.018 2 2 0 0 0-1.612 1.612c-.028.154-.032.36-.032 1.04V19.4a8.194 8.194 0 0 0 .011.588l.014.002c.116.01.278.01.575.01h10.8a8.207 8.207 0 0 0 .589-.012v-.013c.01-.116.011-.279.011-.575v-1.721c0-.674-.004-.88-.032-1.032a2 2 0 0 0-1.614-1.615 1.443 1.443 0 0 0-.146-.018 2.114 2.114 0 0 0-.337-.014.97.97 0 0 0-.1.01m-.096.024-.045.017a4.473 4.473 0 0 0-.154.064c-.139.06-.325.144-.59.264-.881.4-1.86.621-2.886.621a6.978 6.978 0 0 1-2.886-.62c-.265-.12-.451-.205-.59-.265a4.47 4.47 0 0 0-.154-.064l-.045-.017h.002a1.008 1.008 0 0 0-.098-.023 1.024 1.024 0 0 0-.1-.011m-.459-1.982c.2-.016.381-.03.608-.013.223.017.376.044.591.105.221.062.544.209.962.398l.108.05A4.98 4.98 0 0 0 12 14a4.979 4.979 0 0 0 2.168-.492c.419-.19.742-.336.962-.398.215-.06.369-.088.592-.105.226-.017.407-.003.608.013l.03.002c.117.009.231.022.347.043a4 4 0 0 1 3.23 3.23c.063.353.063.753.063 1.292v1.847c0 .252 0 .498-.017.706a2.022 2.022 0 0 1-.201.77 2 2 0 0 1-.874.874 2.022 2.022 0 0 1-.77.201c-.208.017-.454.017-.706.017H6.568c-.252 0-.498 0-.706-.017a2.022 2.022 0 0 1-.77-.201 2 2 0 0 1-.874-.874 2.022 2.022 0 0 1-.201-.77C4 19.93 4 19.684 4 19.432V17.59c0-.544 0-.947.064-1.303a4 4 0 0 1 3.223-3.223 3.395 3.395 0 0 1 .383-.046Z"
         clipRule="evenodd"
@@ -141,7 +118,7 @@ export function SearchIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
       <path
-        className="stroke-white dark:stroke-black"
+        className="stroke-white:stroke-black"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
@@ -154,13 +131,24 @@ export function SearchIcon() {
 export function AddIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+      <path className="fill-white" d="M9 1a1 1 0 0 1 2 0v18a1 1 0 1 1-2 0V1Z" />
       <path
-        className="fill-white dark:fill-black"
-        d="M9 1a1 1 0 0 1 2 0v18a1 1 0 1 1-2 0V1Z"
-      />
-      <path
-        className="fill-white dark:fill-black"
+        className="fill-white"
         d="M19 9a1 1 0 1 1 0 2H1a1 1 0 1 1 0-2h18Z"
+      />
+    </svg>
+  );
+}
+
+export function CalendarIcon({ active = false }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+      <path
+        className={`${active ? "stroke-black" : "stroke-gray-600"}`}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M3 10h18M7 3v2m10-2v2M6.2 21h11.6c1.12 0 1.68 0 2.108-.218a2 2 0 0 0 .874-.874C21 19.48 21 18.92 21 17.8V8.2c0-1.12 0-1.68-.218-2.108a2 2 0 0 0-.874-.874C19.48 5 18.92 5 17.8 5H6.2c-1.12 0-1.68 0-2.108.218a2 2 0 0 0-.874.874C3 6.52 3 7.08 3 8.2v9.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874C4.52 21 5.08 21 6.2 21Z"
       />
     </svg>
   );
